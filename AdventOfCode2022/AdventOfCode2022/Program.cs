b8ba01e138154edb5e10d1e94ac2e86a72b0393b
MG Potgieter <potgieter.m@belgiumcampus.ac.za>
2022-12-01 13:47:59 +0200

    Error handling for fun

--- AdventOfCode2022/AdventOfCode2022/Program.cs
+++ AdventOfCode2022/AdventOfCode2022/Program.cs
@@ -3,9 +3,32 @@ using AdventOfCode2022.DayCode;
 
 Console.WriteLine("Hello, Santa's little helper! Welcome! Please make sure input file is present in the same file as the binary in the format 'dayX.in'");
 Console.WriteLine("Please enter the day number: ");
-int day = int.Parse(Console.ReadLine());
+string input = Console.ReadLine() ?? "invalid";
+
+int day;
+if (input == null || string.IsNullOrEmpty(input) || !int.TryParse(input, out day)) {
+    Console.WriteLine($"Bad input! Stopping...");
+    Console.ReadLine();
+    return;
+}
+
+if (!File.Exists(Environment.CurrentDirectory + $"\\Input\\day{day}.in"))
+{
+    Console.WriteLine($"Input file for Day {day} is missing! Stopping...");
+    Console.ReadLine();
+    return;
+}
 
 string text = File.ReadAllText(Environment.CurrentDirectory + $"\\Input\\day{day}.in").Trim();
 var type = Type.GetType("AdventOfCode2022.DayCode.Day" + day.ToString());
-IDayCode dayCode = (IDayCode)Activator.CreateInstance(type);
+
+if (type == null)
+{
+    Console.WriteLine($"No class exists to handle Day {day}! How did I forget to implement it before running this!? Stopping...");
+    Console.ReadLine();
+    return;
+}
+
+IDayCode dayCode = (IDayCode)Activator.CreateInstance(type)!;
+
 dayCode.RunDay(text);
